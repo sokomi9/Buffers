66ba49b6765b7201b367fd606fd071f3429c31db
Abiud Sokomi <abiudsokomi@gmail.com>
2024-01-12 13:53:00 +0300

    my commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const buffer = new Buffer.from("Vishwas", "UTF-8");
+
+// buffer.write("Codevolution");
+// buffer.write("Code");//buffers have limited memory
+// console.log(buffer.toString());
+// console.log(buffer);
+console.log(buffer.toJSON());
